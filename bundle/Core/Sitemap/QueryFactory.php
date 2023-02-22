3eab937218dd0681823f834dc8c8acaa25fb8e7d
Georg Bader <georg.bader@holzweg.com>
2023-02-22 15:18:02 +0100

    bugfix - we want to have multiple content-types

--- bundle/Core/Sitemap/QueryFactory.php
+++ bundle/Core/Sitemap/QueryFactory.php
@@ -114,6 +114,8 @@ final class QueryFactory
             }
             $criterions[] = new Criterion\ContentTypeIdentifier($contentTypeIdentifier);
         }
+        // bugfix - we want to have multiple content-types  
+        $criterions = array(new Criterion\LogicalOr($criterions));
 
         foreach ($subtreeLocationsId as $locationId) {
             $excludedLocation = $this->getLocation($locationId);
